6c52a6acc0d0dc936407dec9366d0ccb30d99ef8
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-02-21 17:14:27 -0600

    feat: add weighting page

--- liftlogger-client/src/app/components/weightscale-data-input/weightscale-data-input.component.ts
+++ liftlogger-client/src/app/components/weightscale-data-input/weightscale-data-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-weightscale-data-input',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./weightscale-data-input.component.sass']
 })
 export class WeightscaleDataInputComponent {
+  data: string = '';
+  @Output() dataChanged = new EventEmitter<number>();
 
+  onDataChanged(): void {
+    this.dataChanged.emit(Number(this.data));
+  }
 }
--- liftlogger-client/src/app/pages/add-weighting/add-weighting.component.ts
+++ liftlogger-client/src/app/pages/add-weighting/add-weighting.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-weighting.component.sass']
 })
 export class AddWeightingComponent {
+
+  data: Record<string, number> = {};
+
+  constructor(private http: HttpClient) { }
+
+  onDataChanged(value: number, fieldName: string): void {
+    this.data[fieldName] = Number(value);
+
+    console.log(this.data)
+  }
+
+  onSubmit(): void {
+    // alert('submitting ' + JSON.stringify(this.data));
+
+
+    this.http.post(
+      'http://localhost:3001/weighting',
+      this.data,
+      { headers: { 'Content-Type': 'application/json' } }
+    ).subscribe();
+  }
 
 }
