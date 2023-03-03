41d23f2c934087d0e457b5f731c9d655c6e27787
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-02 19:06:40 -0600

    doc: documented groups server controller

--- liftlogger-server/src/routes/Groups.ts
+++ liftlogger-server/src/routes/Groups.ts
@@ -5,11 +5,22 @@ import { MuscleGroupCreationParams } from "../models/MuscleGroup";
 
 @Route('groups')
 export class GroupsRoutes extends Controller {
+  /**
+   * Fetches the MuscleGroups DB table for all its content.
+   *
+   * @returns All the Muscle Groups stored in DB.
+   */
   @Get('')
   public async getGroups(): Promise<MuscleGroup[]> {
     return MuscleGroupController.getAll();
   }
 
+  /**
+   * Fetches a single group from DB.
+   *
+   * @param id ID of the group to be fetched.
+   * @returns A group with the fetched `id`. `null` if not found.
+   */
   @Get('{id}')
   public async getGroup(
     @Path() id: number
@@ -17,11 +28,17 @@ export class GroupsRoutes extends Controller {
     return MuscleGroupController.get(id);
   }
 
+  /**
+   * Inserts a new MuscleGroup into DB.
+   *
+   * @param group Group to be created.
+   * @returns DB row for the created group.
+   */
   @Post()
   public async createGroup(
-    @Body() requestBody: MuscleGroupCreationParams
+    @Body() group: MuscleGroupCreationParams
   ): Promise<MuscleGroup> {
-    const newGroup = await MuscleGroupController.createGroup(requestBody)
+    const newGroup = await MuscleGroupController.createGroup(group)
 
     this.setStatus(200);
 
