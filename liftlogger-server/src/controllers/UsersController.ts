b2f1f12583bddfbcfe6f2a19eef3aef575f04fae
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-06 22:28:00 -0600

    feat: server-side user creation implementation

--- liftlogger-server/src/controllers/UsersController.ts
+++ liftlogger-server/src/controllers/UsersController.ts
@@ -3,6 +3,12 @@ import { User, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default class UserController {
+  /**
+   * Fetches a single user from DB.
+   *
+   * @param email User email.
+   * @returns An User object if their email was found in DB.
+   */
   static async get(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: { email: email },
@@ -10,6 +16,12 @@ export default class UserController {
     return user;
   }
 
+  /**
+   * Inserts a new User into the DB.
+   *
+   * @param user User to be inserted.
+   * @returns Newly created user row.
+   */
   static async create(user: User) {
     return await prisma.user.create({
       data: user,
