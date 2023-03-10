faf292ec2a4c2c8deb63444eaf4fdf77f6b6b428
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-09 22:14:44 -0600

    feat: Groups routes now require authentication

--- liftlogger-server/src/routes/GroupsRoutes.ts
+++ liftlogger-server/src/routes/GroupsRoutes.ts
@@ -1,7 +1,11 @@
+import * as express from 'express';
 import { MuscleGroup } from "@prisma/client";
-import { Body, Controller, Get, Path, Post, Query, Route } from "tsoa";
+import { Body, Controller, Get, Middlewares, Path, Post, Request, Route } from "tsoa";
 import MuscleGroupController from "../controllers/MuscleGroupsController";
 import { MuscleGroupCreationParams } from "../models/MuscleGroupModel";
+import { AuthService } from "../services/AuthService";
+import { ModelRequestParams } from "../utils/ModelRequestParams";
+import { shouldBeAuthenticated } from "../middlewares/auth";
 
 @Route('groups')
 export class GroupsRoutes extends Controller {
@@ -11,6 +15,7 @@ export class GroupsRoutes extends Controller {
    * @returns All the Muscle Groups stored in DB.
    */
   @Get('')
+  @Middlewares([shouldBeAuthenticated])
   public async getGroups(): Promise<MuscleGroup[]> {
     return MuscleGroupController.getAll();
   }
@@ -22,6 +27,7 @@ export class GroupsRoutes extends Controller {
    * @returns A group with the fetched `id`. `null` if not found.
    */
   @Get('{id}')
+  @Middlewares([shouldBeAuthenticated])
   public async getGroup(
     @Path() id: number
   ): Promise<MuscleGroup | null> {
@@ -35,10 +41,18 @@ export class GroupsRoutes extends Controller {
    * @returns DB row for the created group.
    */
   @Post()
+  @Middlewares([shouldBeAuthenticated])
   public async createGroup(
-    @Body() group: MuscleGroupCreationParams
-  ): Promise<MuscleGroup> {
-    const newGroup = await MuscleGroupController.createGroup(group)
+    @Body() group: ModelRequestParams<MuscleGroupCreationParams>,
+    @Request() req: express.Request,
+  ): Promise<MuscleGroup | undefined> {
+    if (!req.auth) {
+      return;
+    }
+
+    const { email } = await AuthService.getUserInfo(req.auth.token);
+
+    const newGroup = await MuscleGroupController.createGroup({ ...group, user_email: email })
 
     this.setStatus(200);
 
