0697b2aae0fcce20b498c28ee78053d457d3faa0
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-15 15:57:48 -0500

    fix: makes Weighting user-agnostic

--- liftlogger-server/src/routes/WeightingsRoutes.ts
+++ liftlogger-server/src/routes/WeightingsRoutes.ts
@@ -1,32 +1,58 @@
+import * as express from 'express';
 import { Weighting } from "@prisma/client";
-import { Body, Controller, Get, Post, Route } from "tsoa";
+import { Body, Controller, Get, Middlewares, Post, Request, Route } from "tsoa";
 import { WeightingController } from "../controllers/WeightingController";
-import { WeightingCreationParams } from "../models/WeightingModel";
+import { shouldBeAuthenticated } from "../middlewares/auth";
+import { WeightingCreationParams, WeightingCreationRequestParams } from "../models/WeightingModel";
+import { ModelRequestParams } from "../utils/ModelRequestParams";
+import { AuthService } from '../services/AuthService';
 
 @Route('weightings')
 export class WeightingRoutes extends Controller {
   /**
    * Gets all the weightings from DB.
    *
-   * @returns All the content from the Weightings DB table.
+   * @param req Request object
+   * @returns All the content from the Weightings DB table
    */
   @Get('')
-  public async getWeightings(): Promise<Weighting[]> {
-    // TODO: Make it user agnostic.
-    return WeightingController.getEntries();
+  @Middlewares([shouldBeAuthenticated])
+  public async getWeightings(
+    @Request() req: express.Request
+  ): Promise<Weighting[] | null | undefined> {
+    if (!req.auth) {
+      return;
+    }
+
+    const { email } = await AuthService.getUserInfo(req.auth.token);
+
+    return WeightingController.getEntries(email);
   }
 
   /**
    * Creates a new entry on the Weighting DB table.
    *
-   * @param weightingData Data to be inserted.
-   * @returns Created row.
+   * @param weightingData Data to be inserted
+   * @param req Request object
+   * @returns Created row
    */
   @Post()
+  @Middlewares([shouldBeAuthenticated])
   public async createWeighting(
-    @Body() weightingData: WeightingCreationParams
-  ): Promise<Weighting> {
-    const newEntry = await WeightingController.createEntry(weightingData);
+    @Body() weightingData: WeightingCreationRequestParams,
+    @Request() req: express.Request
+  ): Promise<Weighting | null | undefined> {
+    if (!req.auth) {
+      return;
+    }
+
+    const { email } = await AuthService.getUserInfo(req.auth.token);
+
+    const newEntry = await WeightingController.createEntry({
+      ...weightingData,
+      user_email: email,
+      datetime: weightingData.datetime ?? Math.round(new Date().getTime() / 1000),
+    });
 
     return newEntry;
   }
