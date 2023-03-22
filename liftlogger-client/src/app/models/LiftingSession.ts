1ab2f700ed491a6ed6c6d3873aa5641ca13781a8
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-22 16:21:57 -0500

    feat: Sessions and Sets models added to client

---
+++ liftlogger-client/src/app/models/LiftingSession.ts
@@ -0,0 +1,36 @@
+import { LiftingSet } from "./LiftingSet";
+
+/**
+ * A weightlifting session.
+ */
+export type LiftingSession = {
+  /**
+   * Identifier for the session.
+   */
+  id?: number;
+
+  /**
+   * When did the session started.
+   */
+  start_time: number;
+
+  /**
+   * When did the session was finished.
+   */
+  end_time: number;
+
+  /**
+   * Notes from the user about this specific session.
+   */
+  notes?: string;
+
+  /**
+   * Owner of this session.
+   */
+  user_email?: string;
+
+  /**
+   * List of sets that belong to this session.
+   */
+  sets: LiftingSet;
+}
---
+++ liftlogger-client/src/app/models/LiftingSet.ts
@@ -0,0 +1,42 @@
+import { LiftingSession } from "./LiftingSession";
+import { Movement } from "./Movement";
+
+/**
+ * Individual set perfomed during a weighlifting session.
+ */
+export type LiftingSet = {
+  /**
+   * Idenfitier for this set in DB.
+   */
+  id?: number;
+
+  /**
+   * Identifier for the movement made in this set.
+   */
+  movement_id?: number;
+
+  /**
+   * Number of repetitions of the movement.
+   */
+  reps: number;
+
+  /**
+   * How much weight used in this movement.
+   */
+  weight: number;
+
+  /**
+   * Identifier for the LiftingSession of which this set belongs to.
+   */
+  session_id?: number;
+
+  /**
+   * LiftingSession this set belongs to.
+   */
+  session?: LiftingSession;
+
+  /**
+   * Movement made during this set.
+   */
+  movement?: Movement;
+}
