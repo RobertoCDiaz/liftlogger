e8932b615815e33ef7fca65499a9b2e2292709d9
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-23 15:07:47 -0500

    feat: MovementJournalEntry component

---
+++ liftlogger-client/src/app/components/movement-journal-entry/movement-journal-entry.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovementJournalEntryComponent } from './movement-journal-entry.component';
+
+describe('MovementJournalEntryComponent', () => {
+  let component: MovementJournalEntryComponent;
+  let fixture: ComponentFixture<MovementJournalEntryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MovementJournalEntryComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MovementJournalEntryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ liftlogger-client/src/app/components/movement-journal-entry/movement-journal-entry.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import * as moment from 'moment';
+import { MovementJournalEntry } from 'src/app/models/MovementJournalEntry';
+
+@Component({
+  selector: 'app-movement-journal-entry',
+  templateUrl: './movement-journal-entry.component.html',
+  styleUrls: ['./movement-journal-entry.component.sass']
+})
+export class MovementJournalEntryComponent implements OnChanges {
+  /**
+   * MovementJournalEntry used to populate the component's view.
+   */
+  @Input() entry: MovementJournalEntry;
+
+  /**
+   * Date of the entry already formatted to be displayed.
+   */
+  entryDate: string;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!this.entry) {
+      return;
+    }
+
+    this.entryDate = moment(this.entry.date * 1000).format('MMMM Do, Y')
+  }
+}
