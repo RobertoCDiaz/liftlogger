eacd1de6771c412b9de11e79de00521ad3faa06b
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-28 21:57:38 -0500

    ui: added a litte graph to landing page

--- liftlogger-client/src/app/pages/landing/landing.component.ts
+++ liftlogger-client/src/app/pages/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { GraphInput } from 'src/app/components/graph/graph.component';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,4 +10,15 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LandingComponent {
   constructor(public authService: AuthService) {}
+
+  /**
+   * Fake data to display in the graph.
+   */
+  data: GraphInput[] = new Array(25).fill(0).map((value, index) => ({
+    data: Math.pow(index, 2),
+    date: moment()
+      .subtract(1, 'month')
+      .add(index + 1, 'days')
+      .toDate(),
+  }));
 }
