bd87fdc564cd62f06a84f6cddc9bece02bc8ad2c
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-03-31 16:19:37 -0500

    test: MovementsJournalsService

--- liftlogger-client/src/app/services/movement-journals.service.spec.ts
+++ liftlogger-client/src/app/services/movement-journals.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MovementJournalsService } from './movement-journals.service';
+import { entriesFixture } from '../fixtures/movements-journals.fixture';
 
 describe('MovementJournalsService', () => {
   let service: MovementJournalsService;
@@ -12,5 +13,26 @@ describe('MovementJournalsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getBestSession', () => {
+    it('should return the session with the highest score', () => {
+      const bestSession = service.getBestSession(entriesFixture);
+      expect(bestSession).toEqual(entriesFixture[0]);
+    });
+  });
+
+  describe('getLastSession', () => {
+    it('should return the most recent session', () => {
+      const lastSession = service.getLastSession(entriesFixture);
+      expect(lastSession).toEqual(entriesFixture[1]);
+    });
+  });
+
+  describe('getPRSession', () => {
+    it('should return the session with the highest weight lifted', () => {
+      const prSession = service.getPRSession(entriesFixture);
+      expect(prSession).toEqual(entriesFixture[2]);
+    });
   });
 });
