cdcb35f051413f1d62006e07a2587eb21b7608c5
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-04-13 18:08:46 -0500

    test: app-button test suite

--- liftlogger-client/src/app/components/button/button.component.spec.ts
+++ liftlogger-client/src/app/components/button/button.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
-
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
@@ -55,5 +54,62 @@ describe('ButtonComponent', () => {
     button.click();
 
     expect(emitted).toBeFalse();
+  });
+
+  it('should not render the icon element if `icon` property is not specified', () => {
+    const iconElement = fixture.debugElement.nativeElement.querySelector('span.material-icons');
+
+    expect(iconElement).toBeFalsy();
+  });
+
+  it('should render the icon element if `icon` property is set', () => {
+    component.icon = 'home';
+    fixture.detectChanges();
+
+    const iconElement = fixture.debugElement.nativeElement.querySelector('#icon');
+
+    expect(iconElement).toBeTruthy();
+  });
+
+  it('should render an <a> tag if an href is set', () => {
+    component.href = '/dashboard';
+    fixture.detectChanges();
+
+    const anchorElement = fixture.debugElement.nativeElement.querySelector('a');
+    const buttonElement = fixture.debugElement.nativeElement.querySelector('button');
+
+    expect(anchorElement).toBeTruthy();
+    expect(buttonElement).toBeFalsy();
+  });
+
+  it('should render an <button> tag if an href is not specified', () => {
+    fixture.detectChanges();
+
+    const anchorElement = fixture.debugElement.nativeElement.querySelector('a');
+    const buttonElement = fixture.debugElement.nativeElement.querySelector('button');
+
+    expect(anchorElement).toBeFalsy();
+    expect(buttonElement).toBeTruthy();
+  });
+
+  it('should be a submit button if isSubmitButton equals true', () => {
+    component.isSubmitButton = true;
+    fixture.detectChanges();
+
+    const buttonElement: HTMLButtonElement =
+      fixture.debugElement.nativeElement.querySelector('button');
+
+    expect(buttonElement.type).not.toBe('button');
+    expect(buttonElement.type).toBe('submit');
+  });
+
+  it('should NOT be a submit button if isSubmitButton equals false', () => {
+    fixture.detectChanges();
+
+    const buttonElement: HTMLButtonElement =
+      fixture.debugElement.nativeElement.querySelector('button');
+
+    expect(buttonElement.type).not.toBe('submit');
+    expect(buttonElement.type).toBe('button');
   });
 });
