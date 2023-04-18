7f2d360e80a4766afe072814ca46c8b7367a39fb
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-04-18 17:13:55 -0500

    test: fixtures for movements and musclegroups

---
+++ liftlogger-client/src/app/fixtures/movements.fixture.ts
@@ -0,0 +1,34 @@
+import { Movement } from '../models/MovementModel';
+
+export const movementsFixture: Movement[] = [
+  {
+    id: 1,
+    name: 'District Metrics Manager',
+    description: 'fugit odio in labore voluptas praesentium odit nemo aspernatur asperiores',
+    user_email: 'testing@test.com',
+    primary_group_id: 2,
+  },
+  {
+    id: 2,
+    name: 'Direct Accounts Technician',
+    description: 'nobis omnis minima explicabo beatae sunt distinctio ullam occaecati maiores',
+    user_email: 'testing@test.com',
+    primary_group_id: 2,
+  },
+  {
+    id: 3,
+    name: 'Internal Communications Representative',
+    description:
+      'consequatur ab quisquam minus inventore suscipit sapiente inventore asperiores sint',
+    user_email: 'testing@test.com',
+    primary_group_id: 1,
+  },
+  {
+    id: 4,
+    name: 'Direct Markets Supervisor',
+    description:
+      'praesentium laboriosam minima consequatur vitae a possimus voluptates rem accusamus',
+    user_email: 'testing@test.com',
+    primary_group_id: 3,
+  },
+];
