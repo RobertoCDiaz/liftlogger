c08db30ac4abe8adeac91c8bc4866ea68f6aad0a
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-04-20 12:18:59 -0500

    test: MenuButtonComponent test suite

--- liftlogger-client/src/app/components/menu-button/menu-button.component.spec.ts
+++ liftlogger-client/src/app/components/menu-button/menu-button.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuButtonComponent } from './menu-button.component';
+import { getElement } from 'src/app/helpers/testing.helper';
 
 describe('MenuButtonComponent', () => {
   let component: MenuButtonComponent;
@@ -18,5 +19,55 @@ describe('MenuButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // const testTexts: string[] = ['Main Feature', 'Dashboard', 'Start workout'];
+  const testCases: { text: string; icon: string; href: string }[] = [
+    {
+      text: 'Main Feature',
+      icon: 'home',
+      href: '/',
+    },
+    {
+      text: 'Dashboard',
+      icon: 'insights',
+      href: '/dashboard',
+    },
+    {
+      text: 'Start workout',
+      icon: 'back',
+      href: '/workout',
+    },
+  ];
+
+  it("should render the button's text", () => {
+    testCases.forEach(testCase => {
+      component.text = testCase.text;
+
+      const textElement: HTMLParagraphElement = getElement(fixture, 'p.content');
+
+      expect(textElement.textContent?.trim()).toBe(testCase.text);
+    });
+  });
+
+  it('should change anchor hrefs', () => {
+    testCases.forEach(testCase => {
+      component.href = testCase.href;
+
+      const anchorElement: HTMLAnchorElement = getElement(fixture, 'a.menu-button');
+
+      // anchor's href is automatically appended to host url (e.g. 'http://localhost:9876/' + what we set)
+      expect(anchorElement.href.endsWith(testCase.href)).toBeTrue();
+    });
+  });
+
+  it('should change button icon', () => {
+    testCases.forEach(testCase => {
+      component.icon = testCase.icon;
+
+      const iconElement: HTMLSpanElement = getElement(fixture, 'span');
+
+      expect(iconElement.textContent?.trim()).toBe(testCase.icon);
+    });
   });
 });
--- liftlogger-client/src/app/components/menu-button/menu-button.component.ts
+++ liftlogger-client/src/app/components/menu-button/menu-button.component.ts
@@ -1,13 +1,27 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * Button component to display in the dashboard as a main feature of the app.
+ */
 @Component({
   selector: 'app-menu-button',
   templateUrl: './menu-button.component.html',
   styleUrls: ['./menu-button.component.sass'],
 })
 export class MenuButtonComponent {
+  /**
+   * Text to display.
+   */
   @Input() text: String = 'menu-button';
+
+  /**
+   * Icon to associate to the button. Must be from the MaterialIcons library: https://fonts.google.com/icons.
+   */
   @Input() icon: String = 'home';
+
+  /**
+   * Where to redirect the user when button is clicked.
+   */
   @Input() href: String = '/';
 
   MenuButtonComponent() {}
