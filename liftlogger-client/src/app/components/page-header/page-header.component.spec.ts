06d1901599ae018769d7d5fbba835c4fd0b3ccc6
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-04-26 12:36:39 -0500

    test: PageHeaderComponent test suite

--- liftlogger-client/src/app/components/page-header/page-header.component.spec.ts
+++ liftlogger-client/src/app/components/page-header/page-header.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageHeaderComponent } from './page-header.component';
+import { Location } from '@angular/common';
+import { getElement } from 'src/app/helpers/testing.helper';
 
 describe('PageHeaderComponent', () => {
   let component: PageHeaderComponent;
   let fixture: ComponentFixture<PageHeaderComponent>;
+  let locationService: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,9 +17,30 @@ describe('PageHeaderComponent', () => {
     fixture = TestBed.createComponent(PageHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    locationService = TestBed.inject(Location);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the page title', () => {
+    const testTitle: string = 'This title will be used for testing';
+
+    component.pageTitle = testTitle;
+
+    const titleEl: HTMLParagraphElement = getElement(fixture, '#title');
+
+    expect(titleEl.textContent?.trim()).toBe(testTitle);
+  });
+
+  it('should trigger location.back when back button is pressed', () => {
+    const spy = spyOn(locationService, 'back');
+
+    const backButton: HTMLSpanElement = getElement(fixture, '#back-button');
+    backButton.click();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
