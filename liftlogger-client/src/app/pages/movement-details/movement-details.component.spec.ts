f6035e9abc2c2ec5a7ef707db6465d39937b24af
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-01 11:37:44 -0500

    test: MovementDetails page test suite

--- liftlogger-client/src/app/pages/movement-details/movement-details.component.spec.ts
+++ liftlogger-client/src/app/pages/movement-details/movement-details.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovementDetailsComponent } from './movement-details.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, convertToParamMap } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from 'src/environment/environment';
@@ -10,10 +10,19 @@ import { ContentLoaderComponent } from 'src/app/components/content-loader/conten
 import { GraphComponent } from 'src/app/components/graph/graph.component';
 import { MovementJournalEntryComponent } from 'src/app/components/movement-journal-entry/movement-journal-entry.component';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { MovementsService } from 'src/app/services/movements.service';
+import { of } from 'rxjs';
+import { movementsFixture } from 'src/app/fixtures/movements.fixture';
+import { entriesFixture } from 'src/app/fixtures/movements-journals.fixture';
+import * as moment from 'moment';
 
 describe('MovementDetailsComponent', () => {
   let component: MovementDetailsComponent;
   let fixture: ComponentFixture<MovementDetailsComponent>;
+
+  let activatedRoute: ActivatedRoute;
+  let router: Router;
+  let movementsService: MovementsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -42,9 +51,88 @@ describe('MovementDetailsComponent', () => {
     fixture = TestBed.createComponent(MovementDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+    movementsService = TestBed.inject(MovementsService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to /movements if no ID is specified', () => {
+    const routerSpy = spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    expect(routerSpy).toHaveBeenCalledWith(['movements']);
+  });
+
+  it('should try to fetch movement from the service', () => {
+    const testId = 3;
+    const getMovementsSpy = spyOn(movementsService, 'getMovement').and.returnValue(
+      of(movementsFixture[0]),
+    );
+    spyOnProperty(activatedRoute, 'paramMap').and.returnValue(
+      of(convertToParamMap({ id: testId })),
+    );
+
+    component.ngOnInit();
+
+    expect(getMovementsSpy).toHaveBeenCalledWith(testId);
+  });
+
+  // TEST: should redirect to /movements if fetched movement doesnt exist
+  // it('should redirect to /movements if fetched movement doesnt exist', () => {
+  //   spyOnProperty(activatedRoute, 'paramMap').and.returnValue(of(convertToParamMap({ id: 123 })));
+  //   const getMovementsSpy = spyOn(movementsService, 'getMovement').and.returnValue(of(null));
+  // });
+
+  it('should get movement journal if fetched movement exists', () => {
+    const testId = 5;
+    const serviceSpy = spyOn(movementsService, 'getMovementJournal').and.returnValue(of([]));
+    const functionSpy = spyOn(component, 'setupJournalInformation');
+    spyOnProperty(activatedRoute, 'paramMap').and.returnValue(
+      of(convertToParamMap({ id: testId })),
+    );
+    spyOn(movementsService, 'getMovement').and.returnValue(of(movementsFixture[0]));
+
+    component.ngOnInit();
+
+    expect(serviceSpy).toHaveBeenCalledWith(testId);
+    expect(functionSpy).toHaveBeenCalled();
+  });
+
+  describe('setupJournalInformation()', () => {
+    it('should correctly set the lastSession', () => {
+      const expected = entriesFixture[0];
+
+      component.setupJournalInformation(entriesFixture);
+
+      expect(component.lastSession).toEqual(expected);
+    });
+
+    it('should correctly set the bestSession', () => {
+      const expected = entriesFixture[1];
+
+      component.setupJournalInformation(entriesFixture);
+
+      expect(component.bestSession).toEqual(expected);
+    });
+    it('should correctly set the prSet session', () => {
+      const expected = entriesFixture[2];
+
+      component.setupJournalInformation(entriesFixture);
+
+      expect(component.prSet).toEqual(expected);
+    });
+    it('should correctly set the last trained date (in DD/MM/YYYY format)', () => {
+      const expected = moment(entriesFixture[0].date).format('DD/MM/YYYY');
+
+      component.setupJournalInformation(entriesFixture);
+
+      expect(component.lastTrainedDate).toEqual(expected);
+    });
   });
 });
