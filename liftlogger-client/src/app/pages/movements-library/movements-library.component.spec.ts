1948098854398ee4a769ec58fdf4668e48f181f5
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-02 12:38:32 -0500

    test: MovementLibrary page test suite

--- liftlogger-client/src/app/pages/movements-library/movements-library.component.spec.ts
+++ liftlogger-client/src/app/pages/movements-library/movements-library.component.spec.ts
@@ -4,6 +4,7 @@ import { MovementsLibraryComponent } from './movements-library.component';
 import { DrawerPageComponent } from 'src/app/components/drawer-page/drawer-page.component';
 import { MovementsPickerComponent } from 'src/app/components/movements-picker/movements-picker.component';
 import { AppModule } from 'src/app/app.module';
+import { getElement } from 'src/app/helpers/testing.helper';
 
 describe('MovementsLibraryComponent', () => {
   let component: MovementsLibraryComponent;
@@ -22,5 +23,11 @@ describe('MovementsLibraryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a movements picker', () => {
+    const picker = getElement(fixture, 'app-movements-picker');
+
+    expect(picker).toBeTruthy();
   });
 });
