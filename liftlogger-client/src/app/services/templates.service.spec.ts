52630d3c04f5654ca4d6bb373cedb2fb7e93a6d4
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-17 13:30:58 -0500

    feat: adds a TemplatesService to client

---
+++ liftlogger-client/src/app/services/templates.service.spec.ts
@@ -0,0 +1,65 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TemplatesService } from './templates.service';
+import { HttpService } from './http.service';
+import { getNewTemplateFixture, getTemplatesFixture } from '../fixtures/templates.fixture';
+import { of } from 'rxjs';
+import { AppModule } from '../app.module';
+
+describe('TemplatesService', () => {
+  let service: TemplatesService;
+
+  let http: HttpService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+    });
+    service = TestBed.inject(TemplatesService);
+
+    http = TestBed.inject(HttpService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getUserTemplates()', () => {
+    it('should make a get request to fetch user templates at the /templates endpoint', () => {
+      const testTemplates = getTemplatesFixture();
+      const spy = spyOn(http, 'get').and.returnValue(of(testTemplates));
+
+      service.getUserTemplates().subscribe(result => {
+        expect(spy).toHaveBeenCalledWith('templates');
+        expect(result).toEqual(testTemplates);
+      });
+    });
+  });
+
+  describe('createTemplate()', () => {
+    it('should make a post request to create a new template', () => {
+      const testTemplate = getNewTemplateFixture();
+      const testIds = [1, 4, 6];
+      const spy = spyOn(http, 'post').and.returnValue(of(testTemplate));
+
+      service.createTemplate(testTemplate, testIds).subscribe(result => {
+        expect(spy).toHaveBeenCalledWith('templates', {
+          template: testTemplate,
+          movements_ids: testIds,
+        });
+        expect(result.name).toBe(testTemplate.name);
+        expect(result.description).toBe(testTemplate.description!);
+      });
+    });
+  });
+
+  describe('getMuscleGroupsFromTemplate()', () => {
+    it('should get the names of the muscle groups contained in a template', () => {
+      const testTemplate = getTemplatesFixture()[2];
+
+      const result = service.getMuscleGroupsFromTemplate(testTemplate);
+
+      expect(result).toEqual(new Set(['Chest', 'Back']));
+    });
+  });
+});
---
+++ liftlogger-client/src/app/services/templates.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+import {
+  Template,
+  TemplateCreationParams,
+  TemplateCreationRequestParams,
+} from '../models/TemplateModel';
+
+/**
+ * Provides general utility methods to deal with Templates.
+
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class TemplatesService {
+  http: HttpService = inject(HttpService);
+
+  /**
+   * Makes a request to the server asking for the templates owned by the current user.
+   *
+   * @returns The list of the user's templates
+   */
+  getUserTemplates(): Observable<Template[]> {
+    return this.http.get<Template[]>('templates');
+  }
+
+  /**
+   * Makes a request to the server to create a new Template.
+   *
+   * @param template Data for the new Template
+   * @param movementsIds Identifiers of the movements to be included in the template
+   * @returns The new Template record
+   */
+  createTemplate(template: TemplateCreationParams, movementsIds: number[]): Observable<Template> {
+    return this.http.post<TemplateCreationRequestParams, Template>('templates', {
+      template,
+      movements_ids: movementsIds,
+    });
+  }
+
+  /**
+   * Extracts the names of all the muscle groups that a Template works out.
+   *
+   * @param template Template object
+   * @returns List of the groups' names
+   */
+  getMuscleGroupsFromTemplate(template: Template): Set<string> {
+    const result: Set<string> = new Set();
+
+    template.movements?.forEach(movement => {
+      movement.groups?.forEach(g => {
+        result.add(g.name);
+      });
+    });
+
+    return result;
+  }
+}
