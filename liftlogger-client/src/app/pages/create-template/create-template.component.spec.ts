f81a6c74ae6313a8df5e6f356502b7b6e9ee6959
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-18 14:03:21 -0500

    feat: CreateTemplate page state implementation

--- liftlogger-client/src/app/pages/create-template/create-template.component.spec.ts
+++ liftlogger-client/src/app/pages/create-template/create-template.component.spec.ts
@@ -1,20 +1,101 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CreateTemplateComponent } from './create-template.component';
+import { CreateTemplateComponent, CreateTemplateComponentState } from './create-template.component';
 import { environment } from 'src/environment/environment';
 import { AuthModule } from '@auth0/auth0-angular';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { CreatorPageComponent } from 'src/app/components/creator-page/creator-page.component';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { PageHeaderComponent } from 'src/app/components/page-header/page-header.component';
 import { CreatorInputComponent } from 'src/app/components/creator-input/creator-input.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
 import { TemplateMovementsListComponent } from 'src/app/components/template-movements-list/template-movements-list.component';
 import { TemplateMovementItemComponent } from 'src/app/components/template-movement-item/template-movement-item.component';
+import { BehaviorSubject, of } from 'rxjs';
+import { Movement } from 'src/app/models/MovementModel';
+import { getMovementsFixture } from 'src/app/fixtures/movements.fixture';
+import {
+  CreatorForm,
+  CreatorPageComponent,
+} from 'src/app/components/creator-page/creator-page.component';
+
+describe('CreateTemplateComponent', () => {
+  let state: CreateTemplateComponentState;
+
+  beforeEach(() => {
+    state = new CreateTemplateComponentState();
+  });
+
+  describe('getMovements()', () => {
+    it('should return an observable to movementsSubject', () => {
+      const testMovements = getMovementsFixture();
+      state['movementsSubject'] = new BehaviorSubject<Movement[]>(testMovements);
+
+      state.getMovements().subscribe(result => {
+        expect(result).toEqual(testMovements);
+      });
+    });
+  });
+
+  describe('addMovements()', () => {
+    it('should add movements to state list', () => {
+      const testMovements = getMovementsFixture();
+      const originalMovements = testMovements.slice(0, 2);
+      const newMovement = testMovements[2];
+      const newMovement2 = testMovements[3];
+
+      state['movementsSubject'] = new BehaviorSubject<Movement[]>(originalMovements);
+
+      state.addMovements(newMovement, newMovement2);
+
+      expect(state['movementsSubject'].value).toEqual(testMovements);
+    });
+
+    it('should not add repeated movements', () => {
+      const testMovements = getMovementsFixture();
+      const newMovement = getMovementsFixture()[2];
+
+      state['movementsSubject'] = new BehaviorSubject<Movement[]>(testMovements);
+
+      state.addMovements(newMovement);
+
+      expect(state['movementsSubject'].value).toEqual(testMovements);
+    });
+  });
+
+  describe('removeMovement()', () => {
+    it("should remove a movement from the state's list", () => {
+      const testMovements = getMovementsFixture();
+      const movementToRemove = getMovementsFixture()[1];
+
+      state['movementsSubject'] = new BehaviorSubject<Movement[]>(testMovements);
+
+      state.removeMovement(movementToRemove);
+
+      expect(state['movementsSubject'].value).toEqual(
+        testMovements.filter(m => m.id !== movementToRemove.id),
+      );
+    });
+  });
+
+  describe('moveMovement()', () => {
+    it('should reorder movements', () => {
+      const originalList = getMovementsFixture();
+
+      state['movementsSubject'] = new BehaviorSubject<Movement[]>(
+        JSON.parse(JSON.stringify(originalList)),
+      );
+
+      state.moveMovement(1, 0);
+
+      expect(state['movementsSubject'].value[0]).toEqual(originalList[1]);
+    });
+  });
+});
 
 describe('CreateTemplateComponent', () => {
   let component: CreateTemplateComponent;
   let fixture: ComponentFixture<CreateTemplateComponent>;
+
+  let state: CreateTemplateComponentState;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -39,14 +120,60 @@ describe('CreateTemplateComponent', () => {
           },
         }),
       ],
+      providers: [CreateTemplateComponentState],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreateTemplateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    state = fixture.debugElement.injector.get(CreateTemplateComponentState);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit()', () => {
+    it('should store true if there are movements selected in state', () => {
+      const testMovements = getMovementsFixture();
+      spyOn(state, 'getMovements').and.returnValue(of(testMovements));
+
+      component.ngOnInit();
+
+      component.areMovementsSelected$.subscribe(result => {
+        expect(result).toBeTrue();
+      });
+    });
+
+    it('should store false if there are no movements selected in state', () => {
+      spyOn(state, 'getMovements').and.returnValue(of([]));
+
+      component.ngOnInit();
+
+      component.areMovementsSelected$.subscribe(result => {
+        expect(result).toBeFalse();
+      });
+    });
+  });
+
+  describe('handleFormChanged()', () => {
+    it('should set local form to provided form', () => {
+      const testTitle: string = 'Test Name';
+      const testDescription: string =
+        'This is just some testing information to test the Template creation page';
+
+      const formMock: CreatorForm = new FormGroup({
+        title: new FormControl<string | null>(testTitle, { validators: Validators.required }),
+        description: new FormControl<string | null>(testDescription, {
+          validators: Validators.required,
+        }),
+      });
+
+      component.handleFormChanged(formMock);
+
+      expect(component.templateForm.value.title).toBe(testTitle);
+      expect(component.templateForm.value.description).toBe(testDescription);
+    });
   });
 });
--- liftlogger-client/src/app/pages/create-template/create-template.component.ts
+++ liftlogger-client/src/app/pages/create-template/create-template.component.ts
@@ -1,15 +1,117 @@
-import { Component } from '@angular/core';
+import { moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, Observable, of, switchMap } from 'rxjs';
+import { CreatorForm } from 'src/app/components/creator-page/creator-page.component';
+import { Movement } from 'src/app/models/MovementModel';
+
+/**
+ * Handles State for the whole CreateTemplate page component tree.
+ */
+export class CreateTemplateComponentState {
+  /**
+   * Stores the movements selected to be included in a Template.
+   */
+  private movementsSubject: BehaviorSubject<Movement[]> = new BehaviorSubject<Movement[]>([]);
+
+  /**
+   * Gets the list of selected Movements to be included in the new Template
+   *
+   * @returns Observable instance
+   */
+  getMovements(): Observable<Movement[]> {
+    return this.movementsSubject.asObservable();
+  }
+
+  /**
+   * Adds Movements into the list of selected Movements.
+   *
+   * @param movement Movement reference
+   */
+  addMovements(...movements: Movement[]) {
+    const currentMovements = this.movementsSubject.value;
+
+    for (let movement of movements) {
+      if (currentMovements.find(m => m.id === movement.id)) {
+        continue;
+      }
+
+      currentMovements.push(movement);
+    }
+
+    this.movementsSubject.next(currentMovements);
+  }
+
+  /**
+   * Deletes a Movement from the selected list.
+   *
+   * @param movement Movement reference
+   */
+  removeMovement(movement: Movement) {
+    const currentMovements = this.movementsSubject.value;
+
+    const newArray = currentMovements.filter(m => m.id !== movement.id);
+
+    this.movementsSubject.next(newArray);
+  }
+
+  /**
+   * Repositions a Movement in the state's list. Useful to reorder the list of movements.
+   *
+   * @param originalIndex Index of the Movement to be moved
+   * @param newIndex Index in which the movement will be placed
+   */
+  moveMovement(originalIndex: number, newIndex: number) {
+    const movements = this.movementsSubject.value;
+
+    moveItemInArray(movements, originalIndex, newIndex);
+
+    this.movementsSubject.next(movements);
+  }
+}
 
 @Component({
   selector: 'app-create-template',
   templateUrl: './create-template.component.html',
   styleUrls: ['./create-template.component.sass'],
+  providers: [CreateTemplateComponentState],
 })
-export class CreateTemplateComponent {
-  templateName?: string;
-  templateDescription?: string;
+export class CreateTemplateComponent implements OnInit {
+  /**
+   * Page's state.
+   */
+  state: CreateTemplateComponentState = inject(CreateTemplateComponentState);
 
-  createTemplate(): void {
-    alert(`${this.templateName} : ${this.templateDescription}`);
+  /**
+   * Form tha stores the data for the new Template.
+   */
+  templateForm: CreatorForm = new FormGroup({
+    title: new FormControl<string | null>(null, { validators: Validators.required }),
+    description: new FormControl<string | null>(null, { validators: Validators.required }),
+  });
+
+  /**
+   * Whether there is at least one movement selected or not.
+   */
+  areMovementsSelected$: Observable<boolean>;
+
+  ngOnInit(): void {
+    this.areMovementsSelected$ = this.state.getMovements().pipe(
+      switchMap(movements => {
+        return of(movements.length > 0);
+      }),
+    );
+  }
+
+  createTemplate(): void {}
+
+  /**
+   * Takes in the form from the CreatorPage component and stores its reference to use in this
+   * component.
+   *
+   * @param form CreatorPage form
+   */
+  handleFormChanged(form: CreatorForm) {
+    this.templateForm = form;
   }
 }
