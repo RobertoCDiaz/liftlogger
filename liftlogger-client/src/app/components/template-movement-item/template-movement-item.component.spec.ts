7d6bc9538678994d9d744e1f92cd57b7a225f4af
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-18 14:04:11 -0500

    refactor: improved TemplateMovementItem component

--- liftlogger-client/src/app/components/template-movement-item/template-movement-item.component.spec.ts
+++ liftlogger-client/src/app/components/template-movement-item/template-movement-item.component.spec.ts
@@ -1,22 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TemplateMovementItemComponent } from './template-movement-item.component';
+import { getMovementsFixture } from 'src/app/fixtures/movements.fixture';
+import { getElement } from 'src/app/helpers/testing.helper';
+import { AppModule } from 'src/app/app.module';
+import { CreateTemplateComponentState } from 'src/app/pages/create-template/create-template.component';
 
 describe('TemplateMovementItemComponent', () => {
   let component: TemplateMovementItemComponent;
   let fixture: ComponentFixture<TemplateMovementItemComponent>;
 
+  let state: CreateTemplateComponentState;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TemplateMovementItemComponent],
+      imports: [AppModule],
+      providers: [CreateTemplateComponentState],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TemplateMovementItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    state = fixture.debugElement.injector.get(CreateTemplateComponentState);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display movement name', () => {
+    const testMovement = getMovementsFixture()[0];
+    component.movement = testMovement;
+
+    const nameEl: HTMLSpanElement = getElement(fixture, 'span.name');
+
+    expect(nameEl.textContent?.includes(testMovement.name)).toBeTrue();
+  });
+
+  describe('removeMovement()', () => {
+    it('should call page state to remove current movement', () => {
+      const spy = spyOn(state, 'removeMovement');
+      const testMovement = getMovementsFixture()[0];
+      component.movement = testMovement;
+
+      component.removeMovement();
+
+      expect(spy).toHaveBeenCalledWith(testMovement);
+    });
   });
 });
--- liftlogger-client/src/app/components/template-movement-item/template-movement-item.component.ts
+++ liftlogger-client/src/app/components/template-movement-item/template-movement-item.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Movement } from 'src/app/models/MovementModel';
+import { CreateTemplateComponentState } from 'src/app/pages/create-template/create-template.component';
 
 /**
  * An item for the TemplateMovementList component. It showcases an individual template.
@@ -9,7 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-movement-item.component.sass'],
 })
 export class TemplateMovementItemComponent {
-  onDelete(): void {
-    // TODO: Add implementation for template deletion
+  /**
+   * Page's state.
+   */
+  state: CreateTemplateComponentState = inject(CreateTemplateComponentState);
+
+  /**
+   * Movement to display in the component.
+   */
+  @Input() movement: Movement;
+
+  /**
+   * Removes the current Movement from the page's state.
+   */
+  removeMovement(): void {
+    this.state.removeMovement(this.movement);
   }
 }
