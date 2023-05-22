e268555e94bac36deca55f41918686f0549403f2
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-05-22 12:48:54 -0500

    feat: updateTemplate TemplateController method

---
+++ liftlogger-server/src/controllers/TemplateController.test.ts
@@ -0,0 +1,126 @@
+import { PrismaClient } from '@prisma/client';
+import PrismaUtils from '../utils/PrismaUtils';
+import TemplateController from './TemplateController';
+import { getNewTemplateFixture, getTemplatesFixture } from '../fixtures/TemplateFixtures';
+import { TemplateUpdateParams } from '../models/TemplateModel';
+
+const prismaInstance: PrismaClient = PrismaUtils.getPrismaTestingInstance();
+const controller: TemplateController = new TemplateController(prismaInstance);
+
+describe('TemplateController', () => {
+  afterEach(async () => {
+    await prismaInstance.template.deleteMany({
+      where: { id: { notIn: getTemplatesFixture().map(t => t.id) } },
+    });
+  });
+
+  afterAll(() => {
+    prismaInstance.$disconnect();
+  });
+
+  describe('getTemplates()', () => {
+    it('should return all the templates for a user', async () => {
+      const testEmail = 'testing@test.com';
+      const testTemplates = getTemplatesFixture().filter(
+        template => template.user_email === testEmail,
+      );
+
+      const result = await controller.getTemplates(testEmail);
+
+      expect(result).toHaveLength(testTemplates.length);
+    });
+  });
+
+  describe('create()', () => {
+    it('should create a new template', async () => {
+      const newTemplateData = getNewTemplateFixture();
+      const expectedLength = (await prismaInstance.template.count()) + 1;
+
+      let template = await controller.create(newTemplateData);
+
+      expect(await prismaInstance.template.count()).toBe(expectedLength);
+      expect(template.name).toBe(newTemplateData.name);
+      expect(template.description).toBe(newTemplateData.description);
+    });
+  });
+
+  describe('getTemplate()', () => {
+    it('should return the correct template', async () => {
+      const testTemplate = getTemplatesFixture()[0];
+
+      const result = await controller.getTemplate(testTemplate.id, testTemplate.user_email);
+
+      expect(result?.id).toBe(testTemplate.id);
+      expect(result?.name).toBe(testTemplate.name);
+      expect(result?.description).toBe(testTemplate.description);
+      expect(result?.user_email).toBe(testTemplate.user_email);
+    });
+
+    it('should return null if no template found', async () => {
+      const result = await controller.getTemplate(468, 'not-an-email@testing.com');
+
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('updateTemplate', () => {
+    it('should update a templates information', async () => {
+      const testTemplateData = getNewTemplateFixture();
+      const newData: TemplateUpdateParams = {
+        name: 'This is an updated name',
+        description: 'This is not the original description',
+      };
+
+      let template = await prismaInstance.template.create({ data: testTemplateData });
+
+      expect(template.name).toBe(testTemplateData.name);
+      expect(template.description).toBe(testTemplateData.description);
+
+      await controller.updateTemplate(template.id, newData);
+
+      template = (await prismaInstance.template.findFirst({ where: { id: template.id } }))!;
+
+      expect(template.name).toBe(newData.name);
+      expect(template.description).toBe(newData.description);
+    });
+
+    it('should also be able to update a templates movements', async () => {
+      const testTemplateData = getNewTemplateFixture();
+      const testMovementIds = [1, 2, 3];
+
+      const newData: TemplateUpdateParams = {
+        name: 'This is an updated name',
+        description: 'This is not the original description',
+      };
+      const newMovementIds = [4, 5];
+
+      let template = await prismaInstance.template.create({
+        data: { ...testTemplateData, movements: { connect: testMovementIds.map(id => ({ id })) } },
+      });
+      let movIds: number[] = (
+        await prismaInstance.movement.findMany({
+          select: { id: true },
+          where: { templates: { some: { id: template.id } } },
+        })
+      ).map(mov => mov.id);
+
+      expect(template.name).toBe(testTemplateData.name);
+      expect(template.description).toBe(testTemplateData.description);
+      expect(new Set(movIds)).toEqual(new Set(testMovementIds));
+
+      await controller.updateTemplate(template.id, newData, newMovementIds);
+
+      template = (await prismaInstance.template.findFirst({ where: { id: template.id } }))!;
+      movIds = (
+        await prismaInstance.movement.findMany({
+          select: { id: true },
+          where: { templates: { some: { id: template.id } } },
+        })
+      ).map(mov => mov.id);
+
+      expect(template.name).toBe(newData.name);
+      expect(template.description).toBe(newData.description);
+      expect(new Set(movIds)).toEqual(new Set(newMovementIds));
+    });
+  });
+});
--- liftlogger-server/src/controllers/TemplateController.ts
+++ liftlogger-server/src/controllers/TemplateController.ts
@@ -1,9 +1,9 @@
 import { PrismaClient, Template } from '@prisma/client';
-import { TemplateCreationParams } from '../models/TemplateModel';
-
-const prisma = new PrismaClient();
+import { TemplateCreationParams, TemplateUpdateParams } from '../models/TemplateModel';
 
 export default class TemplateController {
+  constructor(private prisma: PrismaClient) {}
+
   /**
    * Fetches a single template of a user from DB.
    * It also includes the template's movements.
@@ -12,11 +12,8 @@ export default class TemplateController {
    * @param userEmail User email to match it against
    * @returns Fetched template, or nothing if no match was found
    */
-  static async getTemplate(
-    templateId: number,
-    userEmail: string,
-  ): Promise<Template | null | undefined> {
-    const templates = await prisma.template.findFirst({
+  async getTemplate(templateId: number, userEmail: string): Promise<Template | null> {
+    const templates = await this.prisma.template.findFirst({
       where: {
         id: templateId,
         user_email: userEmail,
@@ -36,8 +33,8 @@ export default class TemplateController {
    * @param userEmail Email of the user whose templates are to be fetched.
    * @returns List of the user's templates.
    */
-  static async getTemplates(userEmail: string): Promise<Template[] | null | undefined> {
-    const templates = await prisma.template.findMany({
+  async getTemplates(userEmail: string): Promise<Template[]> {
+    const templates = await this.prisma.template.findMany({
       where: {
         user_email: userEmail,
       },
@@ -61,8 +58,8 @@ export default class TemplateController {
    * @param movementsIds List of the Movements' ids to be included in the template.
    * @returns Template created.
    */
-  static async create(template: TemplateCreationParams, movementsIds?: number[]) {
-    const newTemplate = await prisma.template.create({
+  async create(template: TemplateCreationParams, movementsIds?: number[]): Promise<Template> {
+    const newTemplate = await this.prisma.template.create({
       data: {
         ...template,
         movements: movementsIds && {
@@ -72,5 +69,52 @@ export default class TemplateController {
     });
 
     return newTemplate;
+  }
+
+  /**
+   * Updates the information of Template.
+   *
+   * @param id Identifier for the Template to be modified
+   * @param newTemplateData New data for the Template
+   * @param newMovementsIds New set of Movements identifier to be a part of the template. If not provided, they will not be modified
+   * @returns Updated record
+   */
+  async updateTemplate(
+    id: number,
+    newTemplateData: TemplateUpdateParams,
+    newMovementsIds?: number[],
+  ): Promise<Template> {
+    if (newMovementsIds) {
+      // get current movements ids as list
+      const oldMovementsIds: number[] = (
+        await this.prisma.movement.findMany({
+          select: { id: true },
+          where: { templates: { some: { id } } },
+        })
+      ).map(currTemplate => currTemplate.id);
+
+      // delete relationship between template and its movements
+      await this.prisma.template.update({
+        where: { id },
+        data: {
+          movements: {
+            disconnect: oldMovementsIds.map(id => ({ id })),
+          },
+        },
+      });
+
+      // create relationships between template and new movements
+      await this.prisma.template.update({
+        where: { id },
+        data: { movements: { connect: newMovementsIds.map(id => ({ id })) } },
+      });
+    }
+
+    const updated = await this.prisma.template.update({
+      where: { id },
+      data: newTemplateData,
+    });
+
+    return updated;
   }
 }
