995688d42f17ef394020e9844906167377f02eda
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-06-08 12:40:36 -0500

    feat: StartWorkout page now displays and filters Templates

--- liftlogger-client/src/app/pages/start-workout/start-workout.component.spec.ts
+++ liftlogger-client/src/app/pages/start-workout/start-workout.component.spec.ts
@@ -1,23 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StartWorkoutComponent } from './start-workout.component';
+import { GlobalModule } from 'src/app/modules/global/global.module';
+import { WorkoutModule } from 'src/app/modules/workout/workout.module';
+import { AppModule } from 'src/app/app.module';
+import { TemplatesService } from 'src/app/services/templates.service';
+import { of, skip, take } from 'rxjs';
+import { getTemplatesFixture } from 'src/app/fixtures/templates.fixture';
 
 describe('StartWorkoutComponent', () => {
   let component: StartWorkoutComponent;
   let fixture: ComponentFixture<StartWorkoutComponent>;
 
+  let templateService: TemplatesService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StartWorkoutComponent ]
-    })
-    .compileComponents();
+      imports: [GlobalModule, WorkoutModule, AppModule],
+      declarations: [StartWorkoutComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(StartWorkoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    templateService = TestBed.inject(TemplatesService);
+
+    spyOn(templateService, 'getUserTemplates').and.returnValue(of(getTemplatesFixture()));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngAfterViewInit()', () => {
+    it('should update templates to display based on template search query', () => {
+      spyOn(component.templateSearchBar.queryChanged, 'asObservable').and.returnValue(
+        of('orchest', 'manager', 'should not return a single template'),
+      );
+      component.ngAfterViewInit();
+
+      component.filteredTemplates$.pipe(skip(1), take(1)).subscribe(result => {
+        expect(result).toHaveSize(2);
+      });
+
+      component.filteredTemplates$.pipe(skip(2), take(1)).subscribe(result => {
+        expect(result).toHaveSize(1);
+      });
+
+      component.filteredTemplates$.pipe(skip(3), take(1)).subscribe(result => {
+        expect(result).toHaveSize(0);
+      });
+    });
   });
 });
--- liftlogger-client/src/app/pages/start-workout/start-workout.component.ts
+++ liftlogger-client/src/app/pages/start-workout/start-workout.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, inject } from '@angular/core';
+import { Observable, combineLatest, map, startWith } from 'rxjs';
+import { SearchBarComponent } from 'src/app/components/search-bar/search-bar.component';
+import { Template } from 'src/app/models/TemplateModel';
+import { TemplatesService } from 'src/app/services/templates.service';
 
+/**
+ * Starting page for a new workout session. It offers the possibility to start a new Weightlifting
+ * session from a Template, a Movement, or start a workout using another discipline.
+ */
 @Component({
   selector: 'app-start-workout',
   templateUrl: './start-workout.component.html',
-  styleUrls: ['./start-workout.component.sass']
+  styleUrls: ['./start-workout.component.sass'],
 })
-export class StartWorkoutComponent {
+export class StartWorkoutComponent implements AfterViewInit {
+  templatesService: TemplatesService = inject(TemplatesService);
 
+  /**
+   * SearchBar that will set the query to search Templates.
+   */
+  @ViewChild('templateSearchBar') templateSearchBar: SearchBarComponent;
+
+  /**
+   * Templates to display. They will be filtered using the template search bar query.
+   */
+  filteredTemplates$: Observable<Template[]>;
+
+  ngAfterViewInit(): void {
+    this.filteredTemplates$ = combineLatest([
+      this.templatesService.getUserTemplates(),
+      this.templateSearchBar.queryChanged.asObservable().pipe(startWith('')),
+    ]).pipe(
+      map(([templates, query]) => {
+        return this.templatesService.searchInTemplates(templates, query);
+      }),
+    );
+  }
 }
