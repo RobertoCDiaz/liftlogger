ae4a2a085dcf528dfbf5eb0e4fbb006969f79886
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-06-26 13:51:59 -0500

    chore: deleted old auth middleware

--- liftlogger-server/src/middlewares/auth.ts
+++ liftlogger-server/src/middlewares/auth.ts
@@ -3,15 +3,6 @@ import { auth } from 'express-oauth2-jwt-bearer';
 import { AuthService } from '../services/AuthService';
 
 /**
- * Middleware that will return a 401 if a valid Access token JWT bearer token is not provided in the request.
- */
-export const shouldBeAuthenticated = auth({
-  audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISSUER_URL,
-  tokenSigningAlg: 'RS256',
-});
-
-/**
  * Checks if the request comes from an authenticated user. If so, it fetches the user email
  * and adds it to the `Request` object.
  */
