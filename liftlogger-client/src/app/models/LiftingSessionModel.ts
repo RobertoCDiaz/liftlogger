00bcbf675dc75ce2f62ba2e969df60dd7bc230ec
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-07-05 14:17:27 -0500

    chore: LiftingSession creation and request params on client

--- liftlogger-client/src/app/models/LiftingSessionModel.ts
+++ liftlogger-client/src/app/models/LiftingSessionModel.ts
@@ -1,4 +1,4 @@
-import { LiftingSet } from './LiftingSetModel';
+import { LiftingSet, LiftingSetCreationParams } from './LiftingSetModel';
 
 /**
  * A weightlifting session.
@@ -35,4 +35,24 @@ export type LiftingSessionWithSets = LiftingSession & {
    * List of sets that belong to this session.
    */
   sets: LiftingSet[];
+};
+
+/**
+ * Data required to create a new LiftingSession.
+ */
+export type LiftingSessionCreationParams = Omit<LiftingSession, 'id' | 'user_email'>;
+
+/**
+ * Shape of the body for the POST request to create a new LiftingSession.
+ */
+export type LiftingSessionWithSetsCreationRequestParams = {
+  /**
+   * Session data.
+   */
+  session: LiftingSessionCreationParams;
+
+  /**
+   * List of LiftingSets to include in the session.
+   */
+  sets: LiftingSetCreationParams[];
 };
