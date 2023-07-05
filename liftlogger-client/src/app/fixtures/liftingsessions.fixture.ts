230382dadd4d90cb212c8ba976b1931f2ed5a4fc
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-07-05 14:16:57 -0500

    chore: added sets and sessions fixtures to client

---
+++ liftlogger-client/src/app/fixtures/liftingsessions.fixture.ts
@@ -0,0 +1,124 @@
+import { LiftingSession } from '../models/LiftingSessionModel';
+
+export const liftingSessionsFixture: LiftingSession[] = [
+  {
+    id: 1,
+    start_time: new Date('2023-01-02T15:46:12.167Z'),
+    end_time: new Date('2023-01-02T15:46:12.167Z'),
+    notes:
+      'Quasi excepturi quis doloribus numquam libero deserunt magnam natus tempore. Modi asperiores magni corrupti blanditiis animi cumque. Alias velit repudiandae eum facilis minima sit magni. Distinctio necessitatibus quisquam iste saepe nobis. Ducimus ipsum laboriosam nam hic in.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 2,
+    start_time: new Date('2022-10-27T14:00:20.910Z'),
+    end_time: new Date('2022-10-27T14:00:20.910Z'),
+    notes:
+      'Quaerat repudiandae tempore. Hic laboriosam iure tempora. Delectus similique ratione vel. Laudantium odit saepe rerum saepe.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 3,
+    start_time: new Date('2022-04-28T15:37:56.257Z'),
+    end_time: new Date('2022-04-28T15:37:56.257Z'),
+    notes:
+      'Quas alias corporis. Id magnam cum fuga neque assumenda illum. Totam voluptatem error magnam ipsa commodi ad labore quis soluta. Dicta commodi eveniet quidem blanditiis quisquam labore repellendus autem.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 4,
+    start_time: new Date('2021-12-23T14:44:12.684Z'),
+    end_time: new Date('2021-12-23T14:44:12.684Z'),
+    notes:
+      'Dolor veniam distinctio impedit voluptatibus suscipit dolores perspiciatis error. Soluta at vero incidunt qui quos placeat. Sunt delectus quidem harum eos ea ullam.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 5,
+    start_time: new Date('2021-08-04T06:16:40.102Z'),
+    end_time: new Date('2021-08-04T06:16:40.102Z'),
+    notes:
+      'Modi aspernatur rem totam illo dolor est occaecati. Ipsa eaque voluptas molestias earum debitis officiis natus. Iusto quibusdam voluptates impedit eum.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 6,
+    start_time: new Date('2021-09-20T20:46:32.297Z'),
+    end_time: new Date('2021-09-20T20:46:32.297Z'),
+    notes:
+      'Itaque quia ea molestiae odit repudiandae ipsa. Laboriosam aut ipsum enim incidunt aliquam tempore similique tempore. Id perferendis nihil aliquid omnis. Et itaque aperiam.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 7,
+    start_time: new Date('2022-01-11T14:54:05.864Z'),
+    end_time: new Date('2022-01-11T14:54:05.864Z'),
+    notes:
+      'Cumque dignissimos quis non. Aperiam est beatae beatae accusantium voluptatibus autem earum illum. Deleniti voluptas iste magni nisi placeat excepturi officiis.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 8,
+    start_time: new Date('2023-01-03T07:50:59.795Z'),
+    end_time: new Date('2023-01-03T07:50:59.795Z'),
+    notes:
+      'Nisi vitae veritatis libero quos. Animi optio cumque. Tempora soluta ipsum officia quae officiis quia. Quisquam animi voluptatum odit similique cumque exercitationem molestias. Ipsam dignissimos deleniti facere cumque occaecati placeat.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 9,
+    start_time: new Date('2021-08-19T07:23:19.612Z'),
+    end_time: new Date('2021-08-19T07:23:19.612Z'),
+    notes:
+      'Ut eligendi itaque doloribus ex architecto excepturi atque. Adipisci hic rem. Consequatur sed soluta. Provident distinctio ipsa.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 10,
+    start_time: new Date('2021-12-13T02:14:58.300Z'),
+    end_time: new Date('2021-12-13T02:14:58.300Z'),
+    notes:
+      'Ea rerum dolorum dolorum quidem in dolor quisquam modi qui. Distinctio impedit amet ea. Ea vero animi. Corporis magnam iste modi natus voluptate dolorum voluptate voluptas. Voluptatibus commodi nisi. Officiis esse rem explicabo quasi et quia ipsam excepturi.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 11,
+    start_time: new Date('2022-06-11T09:25:26.689Z'),
+    end_time: new Date('2022-06-11T09:25:26.689Z'),
+    notes:
+      'Aperiam est distinctio molestiae deserunt quaerat. Necessitatibus repellendus natus provident facilis iure. Sapiente illo sint totam quos ducimus nobis vitae dolorum quaerat. Expedita consequuntur qui id necessitatibus. Quisquam maiores fugit harum. Facere odio consequatur dolor quo eaque.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 12,
+    start_time: new Date('2023-01-23T00:07:45.337Z'),
+    end_time: new Date('2023-01-23T00:07:45.337Z'),
+    notes:
+      'Sequi dolor sit ullam nihil ratione ex. Similique ad eius voluptas exercitationem. Quidem nam animi enim aut voluptatibus. Dolorem ducimus aliquid molestias natus. Ea accusantium debitis natus voluptates autem aut commodi. Recusandae est iure repudiandae minima sequi maxime vero.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 13,
+    start_time: new Date('2022-03-29T05:44:53.714Z'),
+    end_time: new Date('2022-03-29T05:44:53.714Z'),
+    notes:
+      'Ex eveniet laudantium architecto doloribus. Deleniti fugit nihil enim numquam voluptatem. Saepe nobis assumenda rem facere minus unde. Molestiae quae eius earum. Reprehenderit quos nesciunt minima iure dolores expedita sint sapiente.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 14,
+    start_time: new Date('2022-02-08T15:15:58.853Z'),
+    end_time: new Date('2022-02-08T15:15:58.853Z'),
+    notes:
+      'Deserunt accusantium pariatur dignissimos nesciunt rem excepturi perferendis aliquid. Labore temporibus laborum quidem blanditiis ut enim. Accusantium libero illo. Harum magnam voluptatem nihil facere vero minima. Blanditiis culpa quod.',
+    user_email: 'testing@test.com',
+  },
+  {
+    id: 15,
+    start_time: new Date('2021-09-05T11:49:11.490Z'),
+    end_time: new Date('2021-09-05T11:49:11.490Z'),
+    notes:
+      'Molestias expedita voluptates libero omnis mollitia. Commodi ratione non ex deserunt. Itaque architecto reprehenderit eos. Voluptatum nulla minima vero distinctio debitis odio. Tempora reprehenderit provident accusamus quos laborum. Natus autem ut fugiat magni ipsum.',
+    user_email: 'testing@test.com',
+  },
+];
