33951c0acf8dd69467e914f7cd1c924c7ae7eca3
Roberto Diaz <robertocdiazsanchez@gmail.com>
2023-07-06 12:33:53 -0500

    feat: createSession method for LiftingSessionsService

--- liftlogger-client/src/app/services/lifting-sessions.service.spec.ts
+++ liftlogger-client/src/app/services/lifting-sessions.service.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LiftingSessionsService } from './lifting-sessions.service';
+import { AppModule } from '../app.module';
+import { LiftingSessionCreationParams } from '../models/LiftingSessionModel';
+import { LiftingSetCreationParams } from '../models/LiftingSetModel';
+import { HttpService } from './http.service';
+import { liftingSessionsFixture } from '../fixtures/liftingsessions.fixture';
+import { of } from 'rxjs';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 
-describe('LiftingSessionsService', () => {
+fdescribe('LiftingSessionsService', () => {
   let service: LiftingSessionsService;
 
+  let http: HttpService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [AppModule],
+    });
     service = TestBed.inject(LiftingSessionsService);
+
+    http = TestBed.inject(HttpService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('createSession()', () => {
+    it('should merge session data and reps and call service to create session with it', () => {
+      const mockSession: LiftingSessionCreationParams = {
+        end_time: new Date('2023-07-05T14:10:39'),
+        start_time: new Date('2023-07-05T13:02:55'),
+        notes: 'Just some notes',
+      };
+      const mockSets: LiftingSetCreationParams[] = [
+        { movement_id: 1, reps: 12, weight: 123 },
+        { movement_id: 1, reps: 11, weight: 123 },
+      ];
+      const mockResult = liftingSessionsFixture[0];
+      const postSpy = spyOn(http, 'post').and.returnValue(of(mockResult));
+
+      const result = service.createSession(mockSession, mockSets);
+
+      const spy = subscribeSpyTo(result);
+
+      expect(spy.getLastValue()).toEqual(mockResult);
+      expect(postSpy).toHaveBeenCalledWith('sessions/', { session: mockSession, sets: mockSets });
+    });
   });
 });
--- liftlogger-client/src/app/services/lifting-sessions.service.ts
+++ liftlogger-client/src/app/services/lifting-sessions.service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LiftingSessionWithSets } from '../models/LiftingSessionModel';
+import {
+  LiftingSession,
+  LiftingSessionCreationParams,
+  LiftingSessionWithSetsCreationRequestParams,
+} from '../models/LiftingSessionModel';
+import { LiftingSetCreationParams } from '../models/LiftingSetModel';
+import { HttpService } from './http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LiftingSessionsService {
-  constructor() {}
+  http: HttpService = inject(HttpService);
 
-  // createSession(): Observable<LiftingSessionWithSets> {
+  /**
+   * Attempts to create a new LiftingSession.
+   *
+   * @param session Data to create the new LiftingSession from
+   * @param sets List of LiftingSets that will belong to the session
+   * @returns New LiftingSession record
+   */
+  createSession(
+    session: LiftingSessionCreationParams,
+    sets: LiftingSetCreationParams[],
+  ): Observable<LiftingSession> {
+    const reqBody: LiftingSessionWithSetsCreationRequestParams = {
+      session: session,
+      sets: sets,
+    };
 
-  // }
+    return this.http.post<LiftingSessionWithSetsCreationRequestParams, LiftingSession>(
+      'sessions/',
+      reqBody,
+    );
+  }
 }
